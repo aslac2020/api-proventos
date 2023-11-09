c2139c0ef6e39a01bf1e9ffd52535b7a628509ff
André Lacerda <andredesenvolvedorti@gmail.com>
2023-11-09 15:33:11 -0300

    implementação do Entity

--- Controllers/EventoController.cs
+++ Controllers/EventoController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Proventos.API.Data;
 using Proventos.API.Models;
 
 namespace Proventos.API.Controllers;
@@ -7,44 +8,23 @@ namespace Proventos.API.Controllers;
 [Route("api/[controller]")]
 public class EventoController : ControllerBase
 {
-    public IEnumerable<Evento> _evento = new Evento[]
+    private readonly DataContext _context;
+
+    public EventoController(DataContext context)
     {
-          new Evento()
-          {
-            EventoId = 1,
-            Tema = "Angular 11 e .NET 6",
-            Local = "São Paulo",
-            Lote = "1 Lote",
-            QtdPessoas = 100,
-            DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-            ImagemURL = "foto.png"
-          },
-          new Evento()
-          {
-            EventoId = 2,
-            Tema = "JavaScript",
-            Local = "São Paulo",
-            Lote = "2 Lote",
-            QtdPessoas = 150,
-            DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-            ImagemURL = "foto.png"
-          }
-    };
- 
-    public EventoController()
-    {
+        _context = context;
     }
 
     [HttpGet]
     public IEnumerable<Evento> Get()
     {
-        return _evento;
+        return _context.Eventos;
       
     }
     [HttpGet("{id}")]
-    public IEnumerable<Evento> GetById(int id)
+    public Evento GetById(int id)
     {
-        return _evento.Where(evento => evento.EventoId == id);
+        return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
 
     }
 
